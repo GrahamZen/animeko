b248f3eb6b5d04366029067face19920f3ff30c1
Gehan Zheng <gehan.zheng@vanderbilt.edu>
2026-02-28 13:11:59 -0600

    build(ci): squash workflow and android-only build updates

--- .github/workflows/src.main.kts
+++ .github/workflows/src.main.kts
@@ -1329,7 +1329,7 @@ class WithMatrix(
         return if (matrix.uploadApk) {
             uses(
                 name = "Prepare signing key",
-                `if` = expr { github.isAnimekoRepository and !github.isPullRequest },
+                `if` = expr { !github.isPullRequest },
                 action = Base64ToFile_Untyped(
                     fileName_Untyped = "android_signing_key",
                     fileDir_Untyped = "./",
@@ -1428,7 +1428,7 @@ class WithMatrix(
         if (matrix.uploadApk) {
             runGradle(
                 name = "Build Android Release APKs",
-                `if` = expr { github.isAnimekoRepository and !github.isPullRequest },
+                `if` = expr { !github.isPullRequest },
                 tasks = arrayOf("assembleDefaultRelease"),
                 env = mapOf(
                     "signing_release_storeFileFromRoot" to expr { prepareSigningKey.outputs.filePath },
